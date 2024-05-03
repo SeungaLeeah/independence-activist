d3635f45967c869546e8734396741340c577eb4e
seunga <seunga_lee@weedscomm.com>
2024-05-03 13:29:38 +0900

    로그 삭제

--- src/app/api/store/data.ts
+++ src/app/api/store/data.ts
@@ -1,7 +1,6 @@
 import { getHonorData } from '../remote/data';
 
 export async function fetchHonorData(params: URLSearchParams) {
-    console.log(params,'params')
     const url = params.toString();
     const data = await getHonorData(url);
     return data;
